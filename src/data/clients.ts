31c9eba6071d902d9c0ad53373a7e1f3f338bae2
Michele Ferracin <michele.ferracin@live.it>
2026-02-28 15:56:19 +0100

    feat: refactor pages to remove unused illustrations and add client data structure

---
+++ src/data/clients.ts
@@ -0,0 +1,20 @@
+export interface Client {
+  name: string;
+  logo: string;
+}
+
+export const clients: Client[] = [
+  { name: 'ROBOMAGISTER', logo: '/images/clients/robomagister.jpg' },
+  { name: 'Oliver James', logo: '/images/clients/oliver-james.gif' },
+  { name: 'Fotolito Graphicolor', logo: '/images/clients/fotolito-graphicolor.png' },
+  { name: 'Imagine Software', logo: '/images/clients/imagine-software.png' },
+  { name: 'iCubed', logo: '/images/clients/icubed.png' },
+  { name: 'MadLab', logo: '/images/clients/madlab.jpg' },
+  { name: 'Logol', logo: '/images/clients/logol.png' },
+  { name: 'Gruppo Lucchi Associati', logo: '/images/clients/gla.png' },
+  { name: 'The Innovation Group', logo: '/images/clients/tig.png' },
+  { name: 'STARCH', logo: '/images/clients/starch.png' },
+  { name: 'e-Project', logo: '/images/clients/eproject.png' },
+  { name: 'UPS Healthcare', logo: '/images/clients/ups-healthcare.png' },
+  { name: 'ASCOM UMS', logo: '/images/clients/ascom-ums.png' },
+];
